28510011ef6e82a2713806daeb477cac50dd00b5
Michael Victor <radmakson@gmail.com>
2023-08-28 13:33:22 +0100

    added: form

--- process.php
+++ process.php
@@ -2,4 +2,9 @@
 
 require_once 'config.php';
 
+// Check if the subscribe button is clicked
+if isset($_POST[subscribe]) {
+    $email = filter_input(INPUT_POST, 'email', SANITIZE_INPUT_EMAIL);
+}
+
 ?>
