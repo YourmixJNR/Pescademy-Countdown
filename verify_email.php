3113e9cead85e4a4c3ad6fdd29fea33cc8679b86
Michael Victor <radmakson@gmail.com>
2023-09-01 16:56:55 +0100

    modified

--- verify_email.php
+++ verify_email.php
@@ -5,7 +5,33 @@ require_once 'config.php';
 // Verify with the unique code
 if(isset($_GET['code'])) {
 
+    // Get the unique code
     $code = $_GET['code'];
+
+    $result = $objDB->query("SELECT * FROM subscribers WHERE reset_code='$code'");
+
+    if($result->num_rows==1) {
+        
+        $data = $result->fetch_object();
+
+        if($data->reset_code=$code) {
+
+            //Update the record of subscriber from is_active 0 to 1 and reset_code to empty
+            $objDB->query("UPDATE subscribers SET is_active=1, reset_code='' WHERE reset_code='$code'");
+
+            //Send email
+            $message = "Hi! You can download your package from <a href='http://localhost/pescademy-countdown/assets/PescaWelcome.png'>here</a>.";
+
+            send_mail([
+                'to' => $data->email,
+                'from' => 'Creative Tools',
+                'message' => $message,
+                'subject' => 'Verify Email'
+            ]);
+
+        }
+
+    }
 
 }
 
